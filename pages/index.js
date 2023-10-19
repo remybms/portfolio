f05fc45d2f210d8dd90f9ef98dd15b43469efa07
remybms <remy.bamas@ynov.com>
2023-10-19 17:03:48 +0200

    start coding page

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,20 @@ import Header from '@/components/header'
 export default function Home() {
   return (
     <>
-    <main className="flex min-h-screen bg-darkblue">
       <Header />
-    </main></>
+      <main>
+        <div className="flex justify-center content-center bg-darkblue text-pink">
+          <div className='grid-cols-1 pt-20'>
+            <div className='text-3xl animate-pulse'>Développeur Web</div>
+            <div>Je suis développeur web, spécialisé en front, passionné et expérimenté.</div>
+            
+          </div>
+          <div className='ml-20'>
+            <img src='/pictures/photo.png' width="250"/>
+          </div>
+        </div>
+
+
+      </main></>
   )
 }
