287e8e9e2a839ac17365f876e4249e53e9c43bfd
remybms <remy.bamas@ynov.com>
2023-12-07 17:07:04 +0100

    start editing portfolio

--- pages/portfolio/index.js
+++ pages/portfolio/index.js
@@ -1,11 +1,38 @@
 import Header from '@/components/header'
 
-export default function Home() {
+export default function Portfolio() {
   return (
     <>
-    <main className="flex min-h-screen bg-darkblue">
+    <main className='min-h-screen bg-darkblue'>
       <Header />
-      portfolio
+      <div className="grid grid-cols-2 justify-around">
+
+        <div className='text-darkpink text-xl m-4'>
+          <h1 className='m-4 w-fit'>
+            Groupie Tracker
+          </h1>
+          <img src='/pictures/groupietracker.png' width={500} />
+          <div className='text-pink mt-4'>
+            Projet avec serveur go se liant à une API.
+          </div>
+          <div className='text-pink'>
+            Réalisé à Nantes Ynov Campus.
+          </div>
+        </div>
+
+        <div className='text-darkpink text-xl m-4'>
+          <h1 className='m-4 w-fit'>
+            Les Briscards
+          </h1>
+          <img src='/pictures/briscards.png' width={500} />
+          <div className='text-pink mt-4'>
+            Projet en Next.js réalisé en groupe.
+          </div>
+          <div className='text-pink'>
+            Réalisé à Nantes Ynov Campus.
+          </div>
+        </div>
+      </div>
     </main></>
   )
 }
