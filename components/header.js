264c31274f328f15c1e4ecc4edf1a564620142a7
remybms <remy.bamas@ynov.com>
2023-12-07 17:06:25 +0100

    edit header

--- components/header.js
+++ components/header.js
@@ -1,8 +1,10 @@
+import Link from "next/link";
+
 export default function Header() {
     return (
-        <div className="flex justify-between max-w-screen bg-darkblue">
+        <div className="flex justify-around max-w-screen bg-darkblue">
         <div className="flex m-8 text-2xl">
-            <div className="text-pink font-montserrat">
+            <div className="text-pink font-montserrat mr-2">
                 Rémy
             </div>
             <div className="text-blue font-montserrat">
@@ -10,9 +12,9 @@ export default function Header() {
             </div>
         </div>
         <div className=" flex m-8 text-2xl h-min text-pink">
-            <a className="hover:border-b-2 border-b-0 border-pastel mr-2" href="contact">Me contacter</a>
-            <a className="hover:border-b-2 border-b-0 border-pastel ml-2 mr-2" href="portfolio">Portfolio</a>
-            <a className="hover:border-b-2 border-b-0 border-pastel ml-2" href="/">à propos</a>
+            <Link className="hover:border-b-2 border-b-0 border-pastel mr-2" href="mailto:bamasremy43@gmail.com">Me contacter</Link>
+            <Link className="hover:border-b-2 border-b-0 border-pastel ml-2 mr-2" href="/portfolio">Portfolio</Link>
+            <Link className="hover:border-b-2 border-b-0 border-pastel ml-2" href="/">à propos</Link>
         </div>
         </div>
 )
