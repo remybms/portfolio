3bc2b784947c362cc4747fbad0473fd45f3792bf
remybms <bamasremy43@gmail.com>
2024-07-10 18:03:12 +0200

    create footer

---
+++ components/footer.js
@@ -0,0 +1,7 @@
+export default function Footer(){
+    return(
+        <div className="flex text-xl text-darkpink max-w-screen justify-center p-8 border-t-2 border-blue">
+            © 2024 Rémy Bamas. Tous droits réservés.
+        </div>
+    )
+}
