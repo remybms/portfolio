998a2f0a1b57b3f8986aa859ee97ea4c63dc0834
remybms <bamasremy43@gmail.com>
2024-07-31 11:37:58 +0200

    add date on blog's articles

--- pages/stageB2/index.js
+++ pages/stageB2/index.js
@@ -24,24 +24,36 @@ export default function Stage() {
 
             <ul className="p-10 space-y-4 text-lg">
                 <li className="flex flex-col space-y-2 items-center">
-                    <h2 className="border-b-2 w-fit text-blue font-semibold text-xl px-1 text-center">
-                        Semaine 1 : Mes débuts à Nantes Ynov Campus !
-                    </h2>
+                    <div className="flex space-x-6 text-blue items-center">
+                        <h2 className="border-b-2 w-fit  font-semibold text-xl px-1 text-center">
+                            Semaine 1 : Mes débuts à Nantes Ynov Campus !
+                        </h2>
+                        <div>
+                            20/07/2024
+                        </div>
+                    </div>
+
                     <p className="text-pastel text-center tablet:mx-20 pc:mx-40">
                         Ma première semaine au sein de Nantes Ynov Campus s'est essentiellement axée sur la prise de poste. On m'a fait une visite des locaux, j'ai pu rencontrer l'équipe mentors de l'école, et plus particulièrement les mentors informatiques qui m'accompagneront tout au long de l'année. Nous avons ensemble commencé à préparer les ymmersions qui auront lieu en septembre pour les premières années. On m'a transmis l'organisation de celles de l'année dernière que je puisse voir ce qui est possible, ce qui ne l'est pas, ce qui a été fait et ce qui doit être enlever.
                     </p>
                 </li>
                 <li className="flex flex-col space-y-2 items-center">
-                    <h2 className="border-b-2 w-fit text-blue font-semibold text-xl px-1 text-center">
-                        Semaine 2
-                    </h2>
+                    <div className="flex space-x-6 text-blue items-center">
+                        <h2 className="border-b-2 w-fit  font-semibold text-xl px-1 text-center">
+                            Semaine 2
+                        </h2>
+                        <div>
+                            27/07/2024
+                        </div>
+                    </div>
+
                     <p className="text-pastel text-center tablet:mx-20 pc:mx-40">
                         Au cours de cette deuxième semaine, j'ai pu assister à une réunion en compagnie du responsable pédagogique, du référent filière et d'un collègue mentor. Au cours de cette réunion, nous avons essentiellement vu le planning des trois semaines d'ymmersions, et nous avons commencé la répartition de présence sur ces journées de cours pour les premières années. Sur cette semaine, j'ai également pu commencer à manipuler les outils nécessaires pour donner cours, tel que ytrack, une plateforme sur laquelle les étudiants pourront s'entrainer à coder avec de nouveaux langages.
                     </p>
                 </li>
 
             </ul>
-            <Footer/>
+            <Footer />
         </main>
     )
 }
