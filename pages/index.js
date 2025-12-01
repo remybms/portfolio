2bae9150c92bb674b907e078bc6bd4d231ad5217
remybms <112947392+remybms@users.noreply.github.com>
2025-12-01 15:57:34 +0100

    change css mode on home page, without the components

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,7 @@ import Work from '@/components/formation/work'
 import School from '@/components/formation/school'
 import Footer from '@/components/footer'
 import Portfolio from '@/components/portfolio';
+import styles from '@/styles/Home.module.css'
 
 export default function Home() {
 
@@ -27,63 +28,59 @@ export default function Home() {
   return (
     <>
 
-      <main className='min-h-screen bg-darkgrey'>
+      <main class={styles.main}>
         <Head>
           <title>Rémy Bamas Lunay</title>
           <link rel="icon" href="/favicon.ico" />
         </Head>
         <Header />
-        <div className="grid grid-cols-1 text-center pc:text-start">
-          <div className='flex flex-col pc:flex-row justify-center items-center content-center text-pink '>
-            <div className='pc:mr-20 pc:mx-5 mb-2 pc:mb-0 justify-center flex tablet:w-2/5 w-3/5 pc:w-1/6 pc:block'>
-              <img src='/pictures/moi.jpg' alt='photo de profil' className='rounded-3xl' />
+        <div class={styles.presentationBloc}>
+          <img src='/pictures/moi.jpg' alt='photo de profil' class={styles.img} />
+          <div class={styles.presentation}>
+            <div class={styles.name}>Rémy Bamas Lunay - Développeur Web</div>
+            <div>
+              <div class={styles.subtitle}>Qui suis-je ?</div>
+              <div class={styles.description}>Je suis un développeur web full stack, passionné et expérimenté.</div>
             </div>
-            <div className='flex flex-col h-fit space-y-5 items-center pc:items-start pc:my-32'>
-              <div className='text-3xl font-bold'>Rémy Bamas Lunay - Développeur Web</div>
-              <div>
-                <div className='text-2xl font-semibold'>Qui suis-je ?</div>
-                <div className='text-xl mx-4 phone:mx-0'>Je suis un développeur web full stack, passionné et expérimenté.</div>
-              </div>
-              <div>
-                <div className='text-2xl font-semibold'>Mes passions</div>
-                <ul className='text-xl list-disc list-inside tablet:ml-5'>
-                  <li>Sports mécaniques</li>
-                  <li>Breakdance</li>
-                </ul>
-              </div>
-              <div className='flex space-x-4'>
-                <Link href='https://www.linkedin.com/in/rémy-bamas-lunay-83ba7a170' target='_blank' className='hover:text-darkpink'><AiFillLinkedin size={50} /></Link>
-                <Link href="https://github.com/remybms" target='_blank'><AiFillGithub size={50} /></Link>
-              </div>
+            <div>
+              <div class={styles.subtitle}>Mes passions</div>
+              <ul class={styles.passionsList}>
+                <li>Sports mécaniques</li>
+                <li>Breakdance</li>
+              </ul>
+            </div>
+            <div class={styles.links}>
+              <Link href='https://www.linkedin.com/in/rémy-bamas-lunay-83ba7a170' target='_blank' class={styles.logo}><AiFillLinkedin size={50} /></Link>
+              <Link href="https://github.com/remybms" target='_blank' class={styles.logo}><AiFillGithub size={50} /></Link>
             </div>
           </div>
         </div>
-        <div id='formation' className='flex flex-col max-w-screen items-center tablet:pt-20'>
-          <ul className='grid grid-cols-2 tablet:grid-cols-3 pc:flex pc:flex-row pc:space-x-10 mb-14 text-darkpink'>
-            <li className='relative flex'>
+        <div id='formation' class={styles.formation}>
+          <ul class={styles.languagesList}>
+            <li class={styles.language}>
               <IoLogoJavascript size={60} className='m-10' />
             </li>
-            <li className='relative flex'>
+            <li class={styles.language}>
               <FaPython size={60} className='m-10 text-blue' />
             </li>
-            <li className='relative flex'>
+            <li class={styles.language}>
               <FaJava size={60} className='m-10' />
             </li>
-            <li className='relative flex'>
+            <li class={styles.language}>
               <SiPhp size={60} className='m-10 text-blue' />
             </li>
             <li className='col-start-1 col-end-3 tablet:col-start-2 tablet:col-end-2 flex justify-center items-center'>
-              <div className='relative flex'>
+              <div class={styles.language}>
                 <SiCsharp size={60} className='m-10' />
               </div>
             </li>
           </ul>
 
-          <div className='text-2xl text-blue p-2 border-4 rounded-lg'>
-            <button onClick={workDisplay} className={` hover:bg-pink hover:text-darkgrey p-2 rounded-md ${experience === 'work' ? 'bg-darkpink text-darkgrey' : 'bg-darkgrey text-pink'}`}>
+          <div class={styles.selector}>
+            <button onClick={workDisplay} class={experience === 'work' ? styles.selected : styles.notselected}>
               Expérience
             </button>
-            <button onClick={schoolDisplay} className={`hover:bg-pink hover:text-darkgrey p-2 rounded-md ${experience === 'school' ? 'bg-darkpink text-darkgrey' : 'bg-darkgrey text-pink'}`}>
+            <button onClick={schoolDisplay} class={experience === 'school' ? styles.selected : styles.notselected}>
               Formation
             </button>
           </div>
