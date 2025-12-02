a50cc058605dbb6fbacdcc40c0b93e64cbb1f75b
remybms <112947392+remybms@users.noreply.github.com>
2025-12-02 13:55:02 +0100

    change css mode on portfolio

--- components/portfolio.js
+++ components/portfolio.js
@@ -1,6 +1,7 @@
 import projects from '@/data/data.json'
 import React from 'react'
 import Link from 'next/link'
+import styles from '@/styles/Portfolio.module.css'
 
 export default function Portfolio() {
 
@@ -13,40 +14,36 @@ export default function Portfolio() {
 
   return (
     <>
-      <main className='flex flex-col bg-darkgrey justify-between'>
-        <div className="grid pc:grid-cols-3 tablet:mx-20 mx-5 grid-cols-1 items-center justify-items-center space-y-5 text-pink">
-          <div className='flex flex-col tablet:space-y-5 space-y-2 text-xl font-semibold justify-self-start'>
-            {data.map((project, index) => (
-              <button key={index} onClick={() => toggleProject(index)} className={`border-b-2 border-transparent hover:border-darkpink px-1 w-fit ${openIndex === index ? 'border-pink' : ''}`}>
-                {project.titre}
-              </button>
-            ))}
-          </div>
-          <div className='pc:col-span-2 pc:flex pc:justify-center'>
-            {data.map((project, index) => (
-              <div key={index} className={`${openIndex === index ? 'flex flex-col pc:w-2/3 justify-self-end items-start space-y-4' : 'hidden'}`}>
-                <img src={project.img} alt={project.titre} className='w-full object-cover' />
-                <div className='grid tablet:grid-cols-[auto_auto] w-full'>
-                  <div className='justify-items-start'>
-                    <div className='text-2xl font-bold text-darkpink'>
-                      {project.titre}
-                    </div>
-                    <p>
-                      {project.description}
-                    </p>
-                    <p>
-                      {project.lieu}
-                    </p>
-                  </div>
-                  <div className='flex h-fit space-x-2 tablet:justify-end'>
-                    {project.github && <Link href={project.github} className='p-2 border-2 rounded-lg px-4' target='_blank'>Github</Link>}
-                    {project.live && <Link href={project.live} className='p-2 border-2 rounded-lg px-4' target='_blank'>Live</Link>}
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
+      <main class={styles.portfolio}>
+        <div class={styles.selector}>
+          {data.map((project, index) => (
+            <button key={index} onClick={() => toggleProject(index)} class={openIndex === index ? styles.selectedTitle : styles.notSelectedTitle}>
+              {project.titre}
+            </button>
+          ))}
         </div>
+        {data.map((project, index) => (
+          <div key={index} className={openIndex === index ? styles.selectedProject : styles.notSelectedProject}>
+            <img src={project.img} alt={project.titre} class={styles.img} />
+            <div class={styles.project}>
+              <div class={styles.description}>
+                <div class={styles.title}>
+                  {project.titre}
+                </div>
+                <p>
+                  {project.description}
+                </p>
+                <p>
+                  {project.lieu}
+                </p>
+              </div>
+              <div class={styles.links}>
+                {project.github && <Link href={project.github} class={styles.link} target='_blank'>Github</Link>}
+                {project.live && <Link href={project.live} class={styles.link} target='_blank'>Live</Link>}
+              </div>
+            </div>
+          </div>
+        ))}
       </main></>
   )
 }
